7c751c00792a29162bfcea321afcc6e2aa90cdfb
Anwesha-kundu <Anweshakundu4954@gmail.com>
2025-04-02 23:59:09 +0530

    Add files via upload

---
+++ Untitled9.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+int main() 
+
+{
+
+   int rows=4;
+
+   for(i=1;i<= rows;i++)
+   {
+
+       for(j=0;j< 3;j++)
+
+       {
+
+         printf("%d",i); 
+
+       }
+
+       printf("\n");
+
+    }
+return 0;
+}
